b5d1631a2892feddfe05fcb3d9a01e8e8c72a749
Arif Arman Pavel <pavel15-5555@diu.edu.bd>
2026-01-27 16:11:31 +0600

    Replace language selector with Google Translate widget in navigation

--- client/src/components/AutoTranslate.tsx
+++ client/src/components/AutoTranslate.tsx
@@ -1,17 +1,10 @@
 import { useEffect } from 'react';
-import { useLanguage } from '@/contexts/LanguageContext';
-import { initGoogleTranslate, changeLanguage } from '@/lib/translationService';
+import { initGoogleTranslate } from '@/lib/translationService';
 
 export default function AutoTranslate() {
-  const { language } = useLanguage();
-
   useEffect(() => {
     initGoogleTranslate();
   }, []);
 
-  useEffect(() => {
-    changeLanguage(language);
-  }, [language]);
-
-  return <div id="google_translate_element" style={{ display: 'none' }} />;
+  return null;
 }
